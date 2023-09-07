ebe346c5d1b64d0d97da6e1a6db65f32c949d41d
nagim <a.nagim@protonmail.com>
2023-09-07 14:27:49 +0200

    last changes

--- recap-project-3_rick-and-morty-app/components/nav-button/nav-button.js
+++ recap-project-3_rick-and-morty-app/components/nav-button/nav-button.js
@@ -1,16 +1,19 @@
-import { cardContainer, nextButton, prevButton, page, pagination } from "../../index.js";
+import {
+  cardContainer,
+  nextButton,
+  prevButton,
+  page,
+  pagination,
+} from "../../index.js";
 
 export function onClick(props) {
   if (props) {
-    return page++
+    return page++;
   } else {
-    return page --
+    return page--;
   }
   page < 42 ? (nextButton.disabled = false) : (nextButton.disabled = true);
   cardContainer.innerHTML = "";
   fetchCharacters();
   pagination.innerHTML = `${page} / ${maxPage}`;
-
 }
-
-
--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -1,20 +1,19 @@
 import { createCharacterCard } from "./components/card/card.js";
-// import { nextButton, prevButton } from "./components/nav-button/nav-button.js";
-import {
-  nextButtonClick,
-  prevButtonClick
-} from "./components/nav-button/nav-button.js";
+import { characterName } from "./components/search-bar/search-bar.js";
 
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
+
 const searchBar = document.querySelector('[data-js="search-bar"]');
+// console.log(searchBar);
+// const searchSumbit = document.querySelector(".search-bar__button");
 
 export const cardContainer = document.querySelector(
   '[data-js="card-container"]'
 );
 
-const navigation = document.querySelector('[data-js="navigation"]');
+// const navigation = document.querySelector('[data-js="navigation"]');
 export const prevButton = document.querySelector('[data-js="button-prev"]');
 export const nextButton = document.querySelector('[data-js="button-next"]');
 export const pagination = document.querySelector('[data-js="pagination"]');
@@ -23,17 +22,13 @@ export const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 42;
 export let page = 1;
 pagination.innerHTML = `${page} / ${maxPage}`;
-const searchQuery = "";
 
+let searchQuery = " ";
 
-
-async function fetchCharacters() {
+async function fetchCharactersName(urL) {
+  let url = urL;
   try {
-    console.log(page);
-    const response = await fetch(
-      `https://rickandmortyapi.com/api/character?page=${page}`
-    );
-    console.log(response.url);
+    const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
       const cards = data.results;
@@ -49,9 +44,32 @@ async function fetchCharacters() {
   }
 }
 
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = event.target.query.value;
+  // console.log(searchQuery);
+  let urL = `https://rickandmortyapi.com/api/character?name=${searchQuery}`;
+  console.log(urL);
+  return fetchCharactersName(urL);
+});
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}?name=${searchQuery}`
+    );
+    if (response.ok) {
+      const data = await response.json();
+      const cards = data.results;
+      cards.forEach((card) => {
+        let cardElement = createCharacterCard(card);
+        cardContainer.append(cardElement);
+      });
+    } else {
+      console.log("Bad response!");
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
 fetchCharacters();
-
-
-
-prevButton.addEventListener("click", onClick)
-nextButton.addEventListener("click", onClick)
